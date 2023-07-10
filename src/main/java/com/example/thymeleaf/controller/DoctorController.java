20da9ee2ba6cc1b7e76819fafee1ab1655c6d279
[dervieux.ben@gmail.com](mailto:dervieux.ben@gmail.com) <dervieux.ben@gmail.com>
2023-07-10 16:16:01 +0200

    quest spring3

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,6 +1,7 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DoctorController {
 
     @GetMapping("/doctor/")
-    public String doctor(@RequestParam(required=false, defaultValue="0") int number,
-                         @RequestParam(required=false, defaultValue="John Smith") String name) {
-
+    public String doctor(@RequestParam(required = false) Integer number,
+            @RequestParam(required = false) String name,
+            Model model) {
+        if (number == null || name == null) {
+            number = 0;
+            name = "John Smith";
+        }
+        model.addAttribute("doctorName", name);
+        model.addAttribute("doctorNumber", number);
         return "doctor";
     }
 }
